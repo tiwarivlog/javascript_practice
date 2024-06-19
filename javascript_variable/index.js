a617dee6e09327846aeb032e6ee1adeac6dc07cb
tiwarivlog <amanuniverse30@gmail.com>
2024-06-19 16:58:27 +0530

    variable defination file added

---
+++ javascript_variable/index.js
@@ -0,0 +1,20 @@
+var firstName = `nikhil`;
+
+let lastName = `tiwari`;
+    
+let age = 25;
+
+let खुश   = true;
+
+age = 45;
+
+let a;
+
+const hoursInDay = 24;
+
+
+let userIntro = 
+`my name is ` +firstName + ' ' + lastName + '. i am ' + age + ' year old';
+
+
+
