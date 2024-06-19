52fbcaa0a5ca62787c2723a6d7fcca051035ba6f
tiwarivlog <amanuniverse30@gmail.com>
2024-06-19 12:17:31 +0530

    javascript discription and dataType

---
+++ Datatype_js/script.js
@@ -0,0 +1,14 @@
+console.log(typeof 1);
+console.log(typeof 'nikhil');
+console.log(typeof parseInt(`vijay`));
+console.log(typeof +`iral`);
+console.log(typeof `100`);
+console.log(typeof `nikhil`+100);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof 11111111111111111111111111111n);
+
+
+
---
+++ Project_1/app.js
@@ -0,0 +1,2 @@
+console.log('Namaste js');
+
