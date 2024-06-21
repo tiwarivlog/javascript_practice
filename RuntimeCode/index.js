b9ba3fa57f1327b76002385aae0dcdca52c7b4c2
tiwarivlog <amanuniverse30@gmail.com>
2024-06-21 17:06:15 +0530

    runTimeMemory file added

---
+++ RuntimeCode/index.js
@@ -0,0 +1,11 @@
+
+console.log(typeof firstName);
+
+var firstName = 'nihkil';
+let lastName ='tiwari';
+let age = 15;
+const yearOfBirth = 1999;
+
+
+// let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName;
+
