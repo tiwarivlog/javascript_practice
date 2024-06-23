cbc488e760d0eb2df10fcb9a5eb62db3c5604121
tiwarivlog <amanuniverse30@gmail.com>
2024-06-23 10:31:13 +0530

    runtime file modified

--- RuntimeCode/index.js
+++ RuntimeCode/index.js
@@ -5,6 +5,7 @@ var firstName = 'nihkil';
 let lastName ='tiwari';
 let age = 15;
 const yearOfBirth = 1999;
+let a = alert("you are welcome");
 
 
 // let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName;
