087d8bad1f8dd5b1f49a8233ceb7c1ca57fa2c31
tiwarivlog <amanuniverse30@gmail.com>
2024-06-26 18:48:50 +0530

    Comprasion operator file added

---
+++ Comprasion_Operator/index.js
@@ -0,0 +1,22 @@
+const user1Age = '18';
+const user2Age = 28;
+const user3Age = 28;
+const user4Age = 38;
+const user5Age = 18;
+
+
+
+console.log(user1Age == user2Age);
+console.log(user2Age == user3Age);
+console.log(user1Age === user2Age);
+console.log(user2Age === user3Age);
+console.log(user3Age != user4Age);
+console.log(user2Age !== user3Age);
+console.log(user2Age > user3Age);
+console.log(user4Age > user2Age);
+console.log(user2Age < user4Age);
+console.log(user2Age <= user4Age);
+console.log(user2Age >= user4Age);
+console.log(80 >= user4Age);
+console.log(parseInt(user1Age) === user5Age);
+
